04f08a836e06611b8f06041b39c42d28c8a9533e
johan <jinfreed23@gmail.com>
2022-08-14 19:40:11 -0500

    Prueba

---
+++ src/main/java/com/ljt/api/WsAlmacenApplication.java
@@ -0,0 +1,13 @@
+package com.ljt.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsAlmacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsAlmacenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ljt/api/WsAlmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljt.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsAlmacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
